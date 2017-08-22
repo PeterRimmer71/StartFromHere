b7bfb65e8d4a1e4863c6ab4ad912b7729c0f5398
PeterRimmer71 <peter.rimmer@ark-energy.eu>
2017-08-22 12:51:09 +0100

    Print out result of the factorial calculation.

--- Program.cs
+++ Program.cs
@@ -11,13 +11,13 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             //test 2
-            int a = 10; // integer variable
+            int a = 5; // integer variable
             PrintMessage(a);
             MyClass x = new MyClass(); // we need it
             ClassTwo xyz = new ClassTwo();  // change to the development branch
             Console.WriteLine("Second line");
             x.PrintBone();    //adding an extra comment to the develop branch while adding features on Feature-X branch!!!
-            x.Factorial(5);
+            Console.WriteLine("Factorial of, {0} is, {1}",a,x.Factorial(a)); // display result
             xyz.PrintIt();    // comment added to the herewego branch
             Console.WriteLine("Random number was, {0}", xyz.Stuff());
             Console.ReadKey();
